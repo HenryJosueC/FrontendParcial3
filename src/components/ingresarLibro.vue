4b7071eebe5d1277d5dc4b0c0debf74a33b9875d
Henry Canastuj <henryjc97@gmail.com>
2020-11-20 19:40:52 -0600

    Consulta para ingresar datos terminada

--- src/components/ingresarLibro.vue
+++ src/components/ingresarLibro.vue
@@ -4,25 +4,24 @@
     <form action="#" class="border p-3 form" style="background-color: #cfd3ce; padding-bottom:100px;"  >
       <div class="form-group">
         <label>Nombre del libro</label>
-        <input type="text" class="form-control" >
+        <input type="text" v-model="titulo" class="form-control" >
       </div>
       <div class="form-group">
         <label >Sinposis</label>
-        <textarea class="form-control" placeholder="Ingrese aqui la información..."></textarea>
+        <textarea class="form-control" v-model="sinopsis" placeholder="Ingrese aqui la información..."></textarea>
       </div>
       <div class="form-group">
         <label>Autor</label>
-        <input type="text" class="form-control">
+        <input type="text" v-model="autor" class="form-control">
       </div>
       <div class="form-group">
         <label>Categoria</label>
-        <select class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
+        <select class="form-control" v-model="Categoria" >
+        <option v-for="cat in allCategorias" :value="cat.id" :key="cat.id">{{cat.name}}</option>
       </select>
       </div>
       <div class="form-group">
-       <button type="submit"  class="btn btn-primary form-control">Guardar</button>
+       <button type="submit" @click="nuevo"  class="btn btn-primary form-control">Guardar</button>
       </div>
       <div class="form-group">
         <label></label>
@@ -34,15 +33,71 @@
 </template>
 
 <script>
+import gql from 'graphql-tag';
+
+const Crate_Libro = gql`
+ mutation crearLibroMutation($titulo: String!, $sinopsis:String!,$autor:String!, $Categoria:Int){
+  crearLibro(
+    titulo:$titulo,
+    sinopsis:$sinopsis,
+    autor:$autor,
+    Categoria:$Categoria
+  )
+  {
+    id
+    titulo
+  }
+}
+`;
+const GET_Libros = gql`
+ query {
+  allCategorias{
+    name
+    id
+  }
+}
+`;
+
 export default {
   data(){
       return{
           titulo:'',
           sinopsis:'',
-          categoria:'',
-          autor:''
+          Categoria:'',
+          autor:'',
+          allCategorias:[]
 
       }
+  },
+  methods:{
+      nuevo (){
+          const titulo= this.titulo
+          const sinopsis= this.sinopsis
+          const autor = this.autor
+          const Categoria= this.Categoria
+          this.$apollo.mutate({
+              mutation: Crate_Libro,
+              variables:{
+                  titulo,
+                  sinopsis,
+                  autor,
+                  Categoria
+              }
+          }).then(function (response) {
+              console.log(response)
+              alert('Ingreso correcto')
+              this.$router.go(-1)
+      })
+        .catch(function (error) {
+          console.log(error)
+        })
+        this.$router.push('/');
+      }
+  },
+  apollo: {
+   allCategorias :{
+     query: GET_Libros
+   }
   }
 }
 </script>
